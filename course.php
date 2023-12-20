e7fead6deff0036a8a4cb50e85e74617b2356f89
Jingman Wang <jwang408@jhu.edu>
2023-12-20 15:50:19 -0500

    pp

--- course.php
+++ course.php
@@ -42,33 +42,32 @@ if (isset($_POST['instructorName'])) {
 
 
 if (isset($_POST['area'])) {
-    $area = $_POST['area'];
+    foreach ((array)$_POST['area'] as $selected) {
+        $area = $selected;
+    }
 } else {
     $area = null;
 }
 
-if (isset($_POST['programOfStudy'])) {
-    foreach ((array)$_POST['programOfStudy'] as $selected) {
-        $programOfStudy = $selected;
-        echo "<h2> $programOfStudy </h2>";
-    }
-} else {
-    $programOfStudy = null;
-}
+// if (isset($_POST['programOfStudy'])) {
+//     foreach ((array)$_POST['programOfStudy'] as $selected) {
+//         $programOfStudy = $selected;
+//         echo "<h2> $programOfStudy </h2>";
+//     }
+// } else {
+//     $programOfStudy = null;
+// }
 
 if (isset($_POST['wi'])) {
-    $writingIntensive = ($_POST['wi'] === 'true');
+    $writingIntensive = ($_POST['wi'] === 'yes')? 1 : 0;
 } else if (isset($_POST['nwi'])) {
-    $writingIntensive = ($_POST['nwi'] === 'false');
+    $writingIntensive = ($_POST['nwi'] === 'no')? 0 : 1;
 } else {
     $writingIntensive = null;
 }
 
-if (isset($_POST['openseat'])) {
-    $openseat = ($_POST['openseat'] === 'yes');
-} else {
-    $openseat = null;
-}
+$openseat = isset($_POST['openseat']) && $_POST['openseat'] === 'true';
+
 
 if (isset($_POST["day"])) {
     $day = $_POST["day"];
@@ -76,8 +75,41 @@ if (isset($_POST["day"])) {
     $day = null;
 }
 
+// $sql = "CALL SearchCourses('$term', '$classTitle', '$classNumber', '$department', '$instructor', '$area', '$writingIntensive', '$openseat', '$day')";
+$sql = "CALL SearchCourses('$classTitle', '$classNumber', '$department')";
+
+$result = mysqli_query($conn, $sql);
+if (!$result) {
+    echo "Query failed\n";
+    echo "Error: " . mysqli_error($conn);
+    exit();
+} else {
+    echo "<table border='1'>";
+    echo "<tr><th>Course ID</th><th>Couse Title</th><th>Credits</th><th>Course Level</th><th>Course Status</th><th>Max Seats</th><th>Open Seats</th><th>Waitlisted</th>
+    <th>Is Writing Intensive</th><th>Course Location</th><th>Course Building</th><th>Area</th></tr>";
+    while ($row = mysqli_fetch_array($result)) {
+        echo "<tr><td>" . $row['CourseName'] . "</td>";
+        echo "<td>" . $row['CourseTitle'] . "</td>";
+        echo "<td>" . $row['Credits'] . "</td>";
+        echo "<td>" . $row['CourseLevel'] . "</td>";
+        echo "<td>" . $row['CourseStatus'] . "</td>";
+        echo "<td>" . $row['MaxSeats'] . "</td>";
+        echo "<td>" . $row['OpenSeats'] . "</td>";
+        echo "<td>" . $row['Waitlisted'] . "</td>";
+        echo "<td>" . $row['IsWritingIntensive'] . "</td>";
+        echo "<td>" . $row['CourseLocation'] . "</td>";
+        echo "<td>" . $row['CourseBuilding'] . "</td>";
+        echo "<td>" . $row['Areas'] . "</td>";
+        // echo "<td>" . $row['DepartmentName'] . "</td>";
+        // echo "<td>" . $row['InstructorName'] . "</td>";
+        // echo "<td>" . $row['DOW'] . "</td>";
+        // echo "<td>" . $row['StartTime'] . "</td>";
+        // echo "<td>" . $row['EndTime'] . "</td></tr>";
+    }
+    echo "</table>";
 
 
+}
 
 echo '<input type="button" value="Back" onclick="history.back(-1); showInputFields()" />';
 
--- course.sql
+++ course.sql
@@ -2,52 +2,54 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS SearchCourses;
 
 CREATE PROCEDURE SearchCourses(
-    IN p_Term VARCHAR(20),
+    -- IN p_Term VARCHAR(20)
     IN p_ClassTitle VARCHAR(255),
     IN p_ClassNumber VARCHAR(255),
-    IN p_Department VARCHAR(255),
+    IN p_Department VARCHAR(255)
     IN p_InstructorName VARCHAR(50),
-    IN p_Area JSON,
-    IN p_WritingIntensive VARCHAR(3),
+    IN p_Area VARCHAR(20)
+    IN p_WritingIntensive TINYINT(1),
     IN p_OpenSeatOnly BOOLEAN,
-    IN p_DaysOfWeek JSON
+
 )
 BEGIN
     SELECT DISTINCT
-        Course.CourseName,
-        Course.CourseTitle,
-        Course.Credits,
-        Course.CourseLevel,
-        Course.CourseStatus,
-        Course.MaxSeats,
-        Course.OpenSeats,
-        Course.Waitlisted,
-        Course.IsWritingIntensive,
-        Couse.CourseLocation,
-        Course.CourseBuilding,
+        c.CourseName,
+        c.CourseTitle,
+        c.Credits,
+        c.CourseLevel,
+        c.CourseStatus,
+        c.MaxSeats,
+        c.OpenSeats,
+        c.Waitlisted,
+        c.IsWritingIntensive,
+        c.CourseLocation,
+        c.CourseBuilding,
+        c.Areas
         Department.DepartmentName,
         Instructor.InstructorName,
         TimeSlot.DOW,
         TimeSlot.StartTime,
         TimeSlot.EndTime
     FROM
-        Course
-    JOIN Term ON Course.TermID = Term.TermID
-    LEFT JOIN InCourseDepartment ON Course.CourseID = InCourseDepartment.CourseID
-    LEFT JOIN Department ON InCourseDepartment.DepartmentID = Department.DepartmentID
-    LEFT JOIN Teaches ON Course.CourseID = Teaches.CourseID
-    LEFT JOIN Instructor ON Teaches.InstructorID = Instructor.InstructorID
-    LEFT JOIN InCourseTimeSlot ON Course.CourseID = InCourseTimeSlot.CourseID
-    LEFT JOIN TimeSlot ON InCourseTimeSlot.TimeSlotID = TimeSlot.TimeSlotID
+        Course c, Term t, InCourseDepartment i, Department d
+    -- LEFT JOIN Teaches ON Course.CourseID = Teaches.CourseID
+    -- LEFT JOIN Instructor ON Teaches.InstructorID = Instructor.InstructorID
+    -- LEFT JOIN TimeSlot ON Course.TimeSlotID = TimeSlot.TimeSlotID
     WHERE
-        (p_Term IS NULL OR p_Term IN (SELECT TermName FROM Term))
-        AND (p_ClassTitle IS NULL OR Course.CourseTitle LIKE CONCAT('%', p_ClassTitle, '%'))
-        AND (p_ClassNumber IS NULL OR Course.CourseName = p_ClassNumber)
-        AND (p_Department IS NULL OR Department.DepartmentName = p_Department)
-        AND (p_InstructorName IS NULL OR Instructor.InstructorName LIKE CONCAT('%', p_InstructorName, '%'))
-        AND (p_Area IS NULL OR JSON_CONTAINS_ARRAY(Course.Area, p_Area))
-        AND (p_WritingIntensive IS NULL OR Course.WritingIntensive = p_WritingIntensive)
-        AND (p_OpenSeatOnly = 0 OR Course.OpenSeats > 0)
-        AND (p_DaysOfWeek IS NULL OR JSON_CONTAINS_ARRAY(TimeSlot.DOW, p_DaysOfWeek));
+    -- p_Area = c.Areas
+     p_ClassNumber = c.CourseName
+    and c.CourseID = i.CourseID
+    AND p_ClassTitle = c.CourseTitle
+    AND p_Department = d.DepartmentName;
+    AND p_InstructorName = Instructor.InstructorName;
+    t.TermName = p_Term
+    AND t.TermID = c.TermID;
+    AND (p_ClassTitle IS NULL OR Course.CourseTitle LIKE CONCAT('%', p_ClassTitle, '%'))
+    AND (p_ClassNumber IS NULL OR Course.CourseName = p_ClassNumber)
+    -- AND (p_Department IS NULL OR Department.DepartmentName = p_Department)
+    -- AND (p_InstructorName IS NULL OR Instructor.InstructorName LIKE CONCAT('%', p_InstructorName, '%'))
+    And (p_area IS NULL OR JSON_SEARCH(Course.Areas, 'one', p_area) IS NOT NULL);
+
 END; //
 DELIMITER ;
